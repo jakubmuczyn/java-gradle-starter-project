3bb2e90e03a3fd12cb777074d9fd6babeb689ea6
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-01-13 12:06:42 +0100

    Changed copyrights - rebase example - feature/SDA-5 branch edit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 Jakub Muczyń - 
+ * Copyright 2019 Jakub Muczyń - Rebase example - feature/SDA-5 branch edit
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
